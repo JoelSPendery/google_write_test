05803ae569ea801acc781fd51bf2bdeaf8c61c33
JoelSPendery <31705930+JoelSPendery@users.noreply.github.com>
2019-03-23 09:53:43 -0400

    Create test_process.R

---
+++ test_process.R
@@ -0,0 +1,19 @@
+## test_process.R - called from Worker VM
+library(googleAuthR)        ## authentication
+library(DBI)                ## to write to mysql database
+
+## authenticate on GCE for google cloud services
+googleAuthR::gar_gce_auth()
+
+my_data <- iris     # get iris data set
+
+# write data to MySQL database
+conn <- dbConnect(
+  RMySQL::MySQL(),
+  username = 'remoteuser',
+  password = 'info123',
+  host = '127.0.0.1',
+  dbname = 'iris'
+)
+
+dbWriteTable(conn, 'test_table', my_data)
